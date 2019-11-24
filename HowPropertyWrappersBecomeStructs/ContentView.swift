64002ce50c6a2e286475469f6a36818c9e8e563e
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-24 11:59:11 +0200

    How property wrappers become struct

--- HowPropertyWrappersBecomeStructs/ContentView.swift
+++ HowPropertyWrappersBecomeStructs/ContentView.swift
@@ -9,8 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount: CGFloat = 0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+        }
     }
 }
 
